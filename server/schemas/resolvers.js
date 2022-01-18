b58756b1ddac77339fd1b029a7376b86e6f07403
Kaden Kelsey <kadenkelsey33@gmail.com>
2022-01-17 20:28:12 -0800

    added connection.js package.json files

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -10,8 +10,33 @@ const resolvers = {
     },
     favorites: async () => {
       return await Favorite.find({});
-    }
-  }
+    },
+  },
+  Mutation: {
+    addProfile: async (parent, { name, email, password }) => {
+      const profile = await Profile.create({ name, email, password });
+      const token = signToken(profile);
+
+      return { token, profile };
+    },
+    login: async (parent, { email, password }) => {
+      const profile = await Profile.findOne({ email });
+
+      if (!profile) {
+        throw new AuthenticationError('No profile with this email found!');
+      }
+
+      const correctPw = await profile.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password!');
+      }
+
+      const token = signToken(profile);
+      return { token, profile };
+    },
+  },
 };
+
 
 module.exports = resolvers;
