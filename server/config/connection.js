bb94e5e47b93af69564d77c1a54168dd3252c982
Chrisaeus <87839602+Chrisaeus@users.noreply.github.com>
2022-01-29 23:37:00 -0800

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/index', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/interactor-db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
