50cb6ddc855f9b35569d7400855433871969f378
Kevin Cortina <kevinc.php@gmail.com>
2022-01-31 21:58:53 -0800

    test

--- server/server.js
+++ server/server.js
@@ -68,9 +68,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, './client/public')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, './client/public')));
+// }
 
 app.use(routes);
 
