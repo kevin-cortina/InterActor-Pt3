7955bd2b7af18b959b9b82d518cb5d1ecb80a8f8
Kevin Cortina <kevinc.php@gmail.com>
2022-01-31 22:36:02 -0800

    cleaned up code

--- client/src/component/ActorFilter.js
+++ client/src/component/ActorFilter.js
@@ -1,17 +1,3 @@
-// import React from 'react';
-
-// export default function ActorFilter({ actorFilters }) {
-//   return (
-//     actorFilters.map((actor) => {
-//       const actorFilterId = 'search-filter-' + actor.id;
-
-//       return (<h4 key={actor.id} className="header chip hoverable actor-filter" id={actorFilterId}>
-//       {actor.name} <button id={actorFilterId} className="btn white gray-text chip close-button">✕</button>
-//   </h4>)
-//   })
-//   )
-// }
-
 import React from 'react';
 
 export default function ActorFilter({ actorFilters }) {
--- client/src/component/SearchFilter.js
+++ client/src/component/SearchFilter.js
@@ -1,36 +1,3 @@
-// import React, { useEffect } from "react";
-// import { useAppContext } from '../assets/script/javascript';
-// import ActorFilter from '../component/ActorFilter';
-// import ResultCard from "./ResultCard";
-
-// function SearchFilter({ currentPage }){
-//     const { appData, actorFilterClicked } = useAppContext();
-
-//     const handleClick = (e) => {
-//         actorFilterClicked(e);
-//     }
-  
-//     useEffect( () => {
-//         // console.log( appData);        
-//     }, [appData]);
-
-//     return(
-//         <div className="row">
-//             <div className="column-1">
-//                 <h4 className="center-align" id="searchFilterTitle">Search Filters</h4>
-//                 <div id="actorFilters" onClick={handleClick}>
-//                     <ActorFilter actorFilters={appData.actorFilters} />
-//                 </div>
-//             </div>
-//             <div className="column-2" id="resultsCol">
-//                 <h4 className="center-align" id="searchFilterResultsTitle">Results</h4>
-//                 <ResultCard movieResultIds={appData.commonMovieIds} />
-//             </div>
-//         </div>
-//     )
-// }
-// export default SearchFilter
-
 import React, { useState, useEffect } from "react";
 import { useAppContext, apiKey, tmdbUrl } from '../assets/script/javascript';
 import ActorFilter from '../component/ActorFilter';
